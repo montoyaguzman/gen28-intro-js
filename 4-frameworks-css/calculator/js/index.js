a6a844dad8031d0048673cce4234e484f3959823
j.montoya <j.montoya@bbva.com>
2023-08-15 21:34:58 -0600

    feat: avance de la calculadora

---
+++ 4-frameworks-css/calculator/js/index.js
@@ -0,0 +1,13 @@
+const num1Element = document.getElementById('num1');
+// document.getElementsByTagName
+// document.getElementsByClassName
+const num2Element = document.querySelector('#num2');
+
+console.log(num1Element);
+console.log(num2Element);
+
+/**
+ * 1. oBTENER EL VALUE de los input y el select (manejo del dom)
+ * 2. Que el boton haga una accion (eventos)
+ * 3. Pintar el resultada en la parte especifica del HTML que queramos (appendChild e inner html)
+ */
